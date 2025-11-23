f8b1b0b20c960be45e499d5dc24a099d4c246d67
kazuki2124 <kazuki010224@icloud.com>
2025-11-23 12:25:02 +0900

    2025/11/23 6回目 ・ジョブの追加 　採掘者と運搬者 ・ジョブ表示用のテキストの追加 ・ジョブごとの作業内容を追加 　運搬者なら運搬を優先、採掘者なら採掘を行う
    # Conflicts:
    #	Assets/Scenes/SampleScene.unity

---
+++ Assets/Scripts/JobSwitchUI.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class JobSwitchUI : MonoBehaviour
+{
+    /// <summary>
+    /// ColonistAIに直接、Jobの変更を行うため
+    /// </summary>
+    public ColonistAI ColonistAI;
+
+    public Button SwitchButton;
+
+    /// <summary>
+    /// Jobの名前を表示するための機能
+    /// </summary>
+    public TextMeshProUGUI JobLabel;
+
+    /// <summary>
+    /// ColonistUIManagerさんから呼ばれることを想定
+    /// </summary>
+    /// <param name="colonistAI"></param>
+    public void SetSwitchUI(ColonistAI colonistAI)
+    {
+        this.ColonistAI = colonistAI;
+        SwitchButton.onClick.AddListener(ToggleJob);
+        UpdateLabel();
+    }
+
+    public void ToggleJob()
+    {
+        // コロニストのJobが採掘者(Miner)だったら
+        if (ColonistAI.Job == ColonistAI.JobType.Miner)
+        {
+            // 運搬者(Carrier)に変更する
+            ColonistAI.Job = ColonistAI.JobType.Carrier;
+        }
+        // コロニストのJobが採掘者(Miner)ではなく運搬者(Carrier)だったら
+        else if (ColonistAI.Job == ColonistAI.JobType.Carrier)
+        {
+            // 採掘者(Miner)に変更する
+            ColonistAI.Job = ColonistAI.JobType.Miner;
+        }
+
+        UpdateLabel();
+    }
+
+    /// <summary>
+    /// JobLabelにColonistAI.Jobの文字を表示する
+    /// </summary>
+    void UpdateLabel()
+    {
+        JobLabel.text = $"job:{ColonistAI.Job}";
+    }
+}
