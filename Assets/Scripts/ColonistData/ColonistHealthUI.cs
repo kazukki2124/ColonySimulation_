0abdf6428d68336873e8be62e2403eb1340acb08
kazuki2124 <kazuki010224@icloud.com>
2025-11-30 12:51:27 +0900

    2025/11/30 7回目_Ver4 ・ストレス値、空腹度のバーを追加 ・ストレス値、空腹度のバーを追加に伴い、体力バーの位置、サイズ調整

--- Assets/Scripts/ColonistData/ColonistHealthUI.cs
+++ Assets/Scripts/ColonistData/ColonistHealthUI.cs
@@ -12,12 +12,28 @@ public class ColonistHealthUI : MonoBehaviour
     /// <summary>
     /// 体力表示用のバー
     /// </summary>
-    public Image healthBar;
+    public Image HealthBar;
+
+    /// <summary>
+    /// ストレス値用のバー
+    /// </summary>
+    public Image StressBar;
+
+    /// <summary>
+    /// 空腹値用のバー
+    /// </summary>
+    public Image HungerBar;
 
     // Update is called once per frame
     void Update()
     {
         // healthBarに現在の体力/最大体力で出る割合を表示する
-        healthBar.fillAmount = ColonistAI.GetCurrentHealth / ColonistAI.MaxHealth;
+        HealthBar.fillAmount = ColonistAI.GetCurrentHealth / ColonistAI.MaxHealth;
+
+        // stressBarに現在のストレス値/100で出る割合を表示する
+        StressBar.fillAmount = ColonistAI.GetStress / 100;
+
+        // stressBarに現在の空腹値/100で出る割合を表示する
+        HungerBar.fillAmount = ColonistAI.GetHunger / 100;
     }
 }
